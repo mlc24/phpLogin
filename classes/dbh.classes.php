915bdfefde1fb7c57dee0b3620dffa139b89aa8c
mlc24 <matheusluca.trabalho@gmail.com>
2024-05-25 02:56:22 -0300

    phpMain

--- classes/dbh.classes.php
+++ classes/dbh.classes.php
@@ -1,7 +1,7 @@
 <?php
 
 class Dbh{
-    private function connect()
+    protected function connect()
     {
         try
         {
--- classes/signup-contr.classes.php
+++ classes/signup-contr.classes.php
@@ -13,6 +13,24 @@ class SignupContr{
         $this->email = $email;
     }
 
+    private function signupUser(){
+        //echo "invalid input";
+        if($this->emptyInput() == false){
+            header("location: ../index.php?error=emptyInput");
+            exit();
+        }
+        //echo "invalid username";
+        if($this->invalidUid() == false){
+            header("location: ../index.php?error=username");
+            exit();
+        }
+        //echo "invalid email";
+        if($this->invalidEmail() == false){
+            header("location: ../index.php?error=email");
+            exit();
+        }
+    }
+
     private function emptyInput() {
         $result;
         if(
@@ -28,29 +46,31 @@ class SignupContr{
     return $result;
     }
 
-private function invalidUid() {
-    $result;
-    if( !preg_match("/^[a-zA-Z0-9]*$/", $this->uid))
-        {
-            $result = false;
+    private function invalidUid() {
+        $result;
+        if( !preg_match("/^[a-zA-Z0-9]*$/", $this->uid))
+            {
+                $result = false;
+            }
+        else{
+            $result = true;
         }
-    else{
-        $result = true;
-    }
-    return $result;
-}
-private function invalidEmail() {
-    $result;
-    if( !filter_var($this->email, FILTER_VALIDATE_EMAIL) )
-        {
-            $result = false;
+        return $result;
         }
-    else
-    {
-        $result = true;
-    }
-    return $result;
-    }
+
+    private function invalidEmail() {
+        $result;
+        if( !filter_var($this->email, FILTER_VALIDATE_EMAIL) )
+            {
+                $result = false;
+            }
+        else
+        {
+            $result = true;
+        }
+        return $result;
+        }
+
     private function pwdMatch() {
         $result;
         if($this->pwd !== $this->pwdRep)
@@ -63,5 +83,18 @@ private function invalidEmail() {
         }
         return $result;
         }
-    
-}
+        
+
+    private function uidTakenCheck() {
+        $result;
+        if(!$this->checkUser($this->uid, $this->email))
+            {
+                $result = false;
+            }
+        else
+        {
+            $result = true;
+        }
+        return $result;
+        }
+    }
